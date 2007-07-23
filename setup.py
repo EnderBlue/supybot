609b03fe5dfd5eece496e379ad3c64b8e173005d
Ben Firshman <phatmonkey@users.sourceforge.net>
2007-07-23 15:10:31 +0000

    Moved /branches/nullmonkey/clones to branches/supybot/clones

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ package_dir = {'supybot': 'src',
 for plugin in plugins:
     package_dir['supybot.plugins.' + plugin] = 'plugins/' + plugin
 
-version = '0.83.2+nullmonkey/clones'
+version = '0.83.2+clones'
 setup(
     # Metadata
     name='supybot',
